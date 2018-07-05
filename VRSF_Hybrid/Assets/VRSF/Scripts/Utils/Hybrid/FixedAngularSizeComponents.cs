f4243df198e02867a26b80733b65ac7664455f61
Thomas Masquart <thomas.masquart@absolute.de>
2018-07-05 14:29:39 +0200

    Refactor the FixedAngularSize

---
+++ VRSF_Hybrid/Assets/VRSF/Scripts/Utils/Hybrid/FixedAngularSizeComponents.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace VRSF.Utils.Hybrid
+{
+    public class FixedAngularSizeComponents : MonoBehaviour
+    {
+        [Header("Off sets the scale ratio so that text does not scale down too much")]
+        [Tooltip("Set to zero for linear scaling")]
+        public float SizeRatio = 0;
+    }
+}
+
---
+++ VRSF_Hybrid/Assets/VRSF/Scripts/Utils/Hybrid/FixedAngularSizeSystem.cs
@@ -0,0 +1,52 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+namespace VRSF.Utils.Hybrid
+{
+     public class FixedAngularSizeSystem : ComponentSystem
+     {
+        private struct Components
+        {
+            public Transform transform;
+            public FixedAngularSizeComponents AngularSize;
+        }
+
+        private float startingDistance;
+        private Vector3 startingScale;
+        private GameObject CameraRig;
+
+        protected override void OnUpdate()
+        {
+            foreach (var comp in GetEntities<Components>())
+            {
+                float distanceToHologram = Vector3.Distance(CameraRig.transform.position, comp.transform.position);
+                float curvedRatio = 1.0f - startingDistance * comp.AngularSize.SizeRatio;
+                comp.transform.localScale = startingScale * (distanceToHologram * comp.AngularSize.SizeRatio + curvedRatio);
+            }
+        }
+
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        private void Start()
+        {
+            foreach (var comp in GetEntities<Components>())
+            {
+                CameraRig = VRSF.Utils.VRSF_Components.CameraRig;
+                // Calculate the XYZ ratios for the transform's localScale over its initial distance from the camera.
+
+                startingDistance = Vector3.Distance(CameraRig.transform.position, comp.transform.position);
+
+                startingScale = comp.transform.localScale;
+
+                if (comp.AngularSize.SizeRatio == 0)
+                {
+                    // Avoid 0 division
+                    if (startingDistance > 0.0f)
+                    {
+                        // Set to a linear scale ratio
+                        comp.AngularSize.SizeRatio = 1.0f / startingDistance;
+                    }
+                }
+            }
+        }
+     }
+}
