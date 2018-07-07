78ed5ca3978adef3756dc81a04fd88d8d94e39c4
Jamy4000 <arnaudbriche1994@gmail.com>
2018-07-07 18:43:05 +0200

    Updated FlyEditor, but we need to find a better version to use the Fly Components

--- VRSF_Hybrid/Assets/VRSF/Scripts/MoveAround/Fly/Editor/FlyEditor.cs
+++ VRSF_Hybrid/Assets/VRSF/Scripts/MoveAround/Fly/Editor/FlyEditor.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEditor;
+using VRSF.MoveAround.Components;
 using VRSF.Utils.Editor;
 
 namespace VRSF.MoveAround.Editor
@@ -6,8 +7,8 @@ namespace VRSF.MoveAround.Editor
     /// <summary>
     /// Just add a message to the user to let him know where the parameters are set.
     /// </summary>
-    [CustomEditor(typeof(Fly))]
-    public class FlyEditor : ButtonActionChoserEditor
+    [CustomEditor(typeof(FlyComponent))]
+    public class FlyEditor : UnityEditor.Editor
     {
         public override void OnInspectorGUI()
         {
@@ -15,6 +16,7 @@ namespace VRSF.MoveAround.Editor
 
             EditorGUILayout.Space();
 
+            EditorGUILayout.HelpBox("To use the Fly System, you only need to set the parameters of the ButtonActionChoserComponents. The rest is already handle in the script.", MessageType.Info);
             EditorGUILayout.HelpBox("The Flying Parameters are set via Scriptable Objects. To modify them, please open Window/VRSF/VR Flying Parameters.", MessageType.Info);
         }
     }
