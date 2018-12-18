c4b14faf786c2780b0b06ceb00d36bf3a1439010
Jamy4000 <arnaudbriche1994@gmail.com>
2018-12-18 16:57:35 +0100

    put back ScriptableToLadAndSave Script

---
+++ VRSF_Hybrid/Assets/ScriptableFramework/Scripts/Utils/ScriptableObject/ScriptableToSaveAndLoad.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace ScriptableFramework.RuntimeSet
+{
+    [CreateAssetMenu(menuName = "RuntimeSet/List/Scriptable To Save-Load")]
+    public class ScriptableToSaveAndLoad : RuntimeSet<ScriptableObject>
+    {
+    }
+}
