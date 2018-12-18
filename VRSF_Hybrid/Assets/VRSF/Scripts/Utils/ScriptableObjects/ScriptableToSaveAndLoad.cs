33d71deb307a1e8f19a83cf62af2ecc2d34e732c
Jamy4000 <arnaudbriche1994@gmail.com>
2018-12-18 15:50:19 +0100

    Restructure project to ease the export

--- VRSF_Hybrid/Assets/VRSF/Scripts/Utils/ScriptableObjects/ScriptableToSaveAndLoad.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-namespace ScriptableFramework.RuntimeSet
-{
-    [CreateAssetMenu(menuName = "RuntimeSet/List/Scriptable To Save-Load")]
-    public class ScriptableToSaveAndLoad : RuntimeSet<ScriptableObject> 
-	{
-    }
-}
